c507f4f4bf95a62b59bce2b801040d3f52206aa1
akash9946276577 <ak0718629@gmail.com>
2021-07-15 21:24:27 +0530

    tkinter container created

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from tkinter import *
+# referal:  https://www.activestate.com/resources/quick-reads/how-to-create-a-calculator-in-python-tkinter/
+
+
+root = Tk()
+
+root.mainloop()
+root.geometry("312x324")
+root.resizable(0, 0)
+root.title("CALCULATOR")
