bcad62ff4ee43f105c63ca4f4075874dd58ac030
akash9946276577 <ak0718629@gmail.com>
2021-07-15 21:37:42 +0530

    tkinter widget functions added

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # referal:  https://www.activestate.com/resources/quick-reads/how-to-create-a-calculator-in-python-tkinter/
 
 
@@ -8,3 +9,26 @@ root.mainloop()
 root.geometry("312x324")
 root.resizable(0, 0)
 root.title("CALCULATOR")
+
+
+def button_click(item):
+    global expression
+    expression + str(item)
+    input_text.set(expression)
+
+
+def button_clear():
+    global expression
+    expression = ""
+    input_text.set("")
+
+
+def button_equal():
+    global expression
+    result = str(eval(expression))
+    input_text.set(result)
+    expression = ""
+
+expression = ""
+
+input_text= StringVar()
